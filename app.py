c020a103e3a696a4e9e23347842c64761a8a5b91
jeironprogrammer <jeironprogrammer@gmail.com>
2025-10-01 15:43:48 +0200

    Alguns canvis estructurals

--- app.py
+++ app.py
@@ -3,23 +3,24 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = ''
+# Aquestes dades haurien d'estar en un fitxer apart "database.py" o ".env"
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = ''
-app.config['MYSQL_PORT'] = 0
+app.config['MYSQL_DB'] = 'gestor_tasques'
+app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-# Configurar els socket només en distribucions linux
-app.config['MYSQL_UNIX_SOCKET'] = '/opt/lampp/var/mysql/mysql.sock'
 
 mysql = MySQL(app)
 
+# Aquesta funció hauria d'estar en un fitxer apart "database.py"
 def crea_tables():
 	with mysql.connection.cursor() as cur:
 		cur.execute("""CREATE TABLE IF NOT EXISTS categoria (id_categoria INT AUTO_INCREMENT PRIMARY KEY, categoria VARCHAR(100) NOT NULL)""");
 		cur.execute("""CREATE TABLE IF NOT EXISTS tasca (id_tasca INT AUTO_INCREMENT PRIMARY KEY, tasca VARCHAR(255) NOT NULL, id_categoria INT NOT NULL, FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria) ON DELETE CASCADE ON UPDATE CASCADE)""");
 		mysql.connection.commit()
 
+# Agafar els dades de la base de dades que es mostressin en el template
 def mostra_tasques():
     tasques_categorias = {}
     with mysql.connection.cursor() as cur:
@@ -30,6 +31,7 @@ def mostra_tasques():
         id_cat = fila['id_categoria']
         if id_cat not in tasques_categorias:
             tasques_categorias[id_cat] = {
+                'id_categoria': fila['id_categoria'],
                 'nom': fila['categoria'],
                 'tasques': []
             }
@@ -42,7 +44,8 @@ def mostra_tasques():
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    crea_tables()
+    crea_tables() # Crear les tables si no existeixen
+
     if request.method == "POST":
         categoria = request.form.get('categoria')
         tasca = request.form.get('tasca')
@@ -60,7 +63,7 @@ def index():
             cur.execute("INSERT INTO tasca (tasca, id_categoria) VALUES (%s, %s)",(tasca, categoria_exist['id_categoria']))
             mysql.connection.commit()
 
-    tasques = mostra_tasques()
+    tasques = mostra_tasques() # Cridar a la funció que retorna els dades a mostrar
     return render_template('index.html', tasques=tasques)
 
 @app.route('/modificar_tasca/<int:id_tasca>', methods=['POST', 'GET'])
@@ -70,7 +73,6 @@ def modificar_tasca(id_tasca):
         with mysql.connection.cursor() as cur:
             cur.execute("UPDATE tasca SET tasca = %s WHERE id_tasca = %s", (tasca, id_tasca))
             mysql.connection.commit()
-        tasques = mostra_tasques()
         return redirect(url_for('index'))
 
 @app.route('/eliminar_tasca/<int:id_tasca>')
@@ -78,7 +80,14 @@ def eliminar_tasca(id_tasca):
     with mysql.connection.cursor() as cur:
         cur.execute("DELETE FROM tasca WHERE id_tasca = %s", (id_tasca,))
         mysql.connection.commit()
-    return index()
+    return redirect(url_for('index'))
+
+@app.route('/eliminar_categoria/<int:id_categoria>')
+def eliminar_categoria(id_categoria):
+    with mysql.connection.cursor() as cur:
+        cur.execute("DELETE FROM categoria WHERE id_categoria = %s", (id_categoria,))
+        mysql.connection.commit()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) # debug = True perquè no esta a producció
